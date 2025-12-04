eb76f66c1b9aa88ef1b78d5c0b92026d4a1c0c64
maycode28 <may.code.28@gmail.com>
2025-12-04 17:50:54 +0900

    recent

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("i = ");
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
 
     }
 }
